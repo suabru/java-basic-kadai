f9ef1c7d9a6ece7cd2b8a47a29b5f44c0bcef279
suabru <sskm01ito27@gmail.com>
2025-02-11 03:50:06 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,12 @@
+package kadai_021;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DictionaryExec_Chapter21 {
+
+    public static void main(String[] args) {
+        List<String> wordsToSearch = Arrays.asList("apple", "banana", "grape", "orange");
+        Dictionary_Chapter21.searchWords(wordsToSearch);
+    }
+}
